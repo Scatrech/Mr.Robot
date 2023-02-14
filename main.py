d89f81283686574247785f983c423c10926b7ead
Scatrech <brandongrancisco@gmail.com>
2023-02-14 09:46:01 -0600

    Datos nuevos
    Cambio de versiones y carpetas
    
    Co-Authored-By: HugoLucas5 <daniellucas10522@gmail.com>

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print(Brandon Francisco Ramirez Saquic, 1527623, Introducción a ingenieria en sistemas)
+
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
+
+
+print('Brandon Francisco Ramírez Saquic, 1527623, Introducción a la ingeniería en Informatica')
+
